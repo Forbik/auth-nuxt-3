cc925e9f1f605c5b9e0ffe09dd00f6a686352a3c
Oleksandr Semenov <forb.alex@gmail.com>
2023-11-01 14:03:12 +0200

    Updates

--- _nuxt/about.f8e13c2e.js
+++
@@ -1,1 +0,0 @@
-import{f as e,o,c as t}from"./entry.55857816.js";const s=e({__name:"about",setup(a){return(n,c)=>(o(),t("div",null,"About Page"))}});export{s as default};
--- _nuxt/index.ed7b8f87.js
+++
@@ -1,1 +0,0 @@
-import{f as e,o as n,c as o}from"./entry.55857816.js";const s=e({__name:"index",setup(t){return(a,c)=>(n(),o("div",null,"Home Page"))}});export{s as default};
