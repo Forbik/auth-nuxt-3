3ad3a5bb6b6a1da27dc47155debe8d831470c08d
Oleksandr Semenov <forb.alex@gmail.com>
2023-11-01 13:29:59 +0200

    Updates

--- _nuxt/auth.883c7a6c.js
+++
@@ -1,1 +0,0 @@
-import{I as i,E as r,J as u}from"./entry.55857816.js";import{u as s,a as t}from"./auth.82ac873c.js";const f=i(e=>{const{authenticated:n}=r(s()),a=t("token");if(a.value&&(n.value=!0),a.value&&(e==null?void 0:e.name)==="login")return u("/");if(!a.value&&(e==null?void 0:e.name)!=="login")return u("/login")});export{f as default};
